c1b66846288fa02b03c35d158fd9e7b147b9a0f7
Alena <alena.belonogova@yandex.ru>
2020-02-27 19:13:35 +0300

    Сделана модель хранения данных от драйвера

--- pweather-station/WSModel.py
+++ pweather-station/WSModel.py
@@ -1,3 +1,77 @@
 class WSModel:
     def __init__(self):
-        pass
+        self._altitude = None
+        self._luminosity = None
+        self._humidity = None
+        self._pressure = None
+        self._temperature = None
+
+    @property
+    def altitude(self) -> float:
+        """
+        :return: Высота от места установки метеостанции, м
+        """
+        return self._altitude
+
+    @altitude.setter
+    def altitude(self, value: float):
+        """
+        :param value: Высота от места установки метеостанции, м
+        """
+        self._altitude = value
+
+    @property
+    def luminosity(self) -> float:
+        """
+        :return: Уровень освещенности, люмен
+        """
+        return self._luminosity
+
+    @luminosity.setter
+    def luminosity(self, value: float):
+        """
+        :param value: Уровень освещенности, люмен
+        """
+        self._luminosity = value
+
+    @property
+    def humidity(self) -> float:
+        """
+        :return: Относительная влажность, %
+        """
+        return self._humidity
+
+    @humidity.setter
+    def humidity(self, value: float):
+        """
+        :param value: Относительная влажность, %
+        """
+        self._humidity = value
+
+    @property
+    def pressure(self) -> float:
+        """
+        :return: Давление, мм.рт.ст
+        """
+        return self._pressure
+
+    @pressure.setter
+    def pressure(self, value: float):
+        """
+        :param value: Давление, мм.рт.ст
+        """
+        self._pressure = value
+
+    @property
+    def temperature(self) -> float:
+        """
+        :return: Температура, *С
+        """
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, value: float):
+        """
+        :param value: Температура, *С
+        """
+        self._temperature = value
